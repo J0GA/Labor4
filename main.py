d03fcba6a4ae511b014475a657b71c1ce20486bf
Stas <SLn00200182@gmail.com>
2022-04-18 17:30:09 +0300

    Task 2

--- main.py
+++ main.py
@@ -159,3 +159,33 @@ for letter in encoded:
     else:
         decoded += letter
 print(decoded)
+
+print()
+print("Задание №3")
+A = Stack()
+B = Stack()
+C = Stack()
+disks = 4
+for i in range(disks, 0, -1):
+    A.push(i)
+def move(a, b):
+    if len(a) == 0 and len(b) > 0:
+        a.push(b.pop())
+    elif len(a) > 0 and len(b) == 0:
+        b.push(a.pop())
+    elif a.peek() > b.peek():
+        a.push(b.pop())
+    else:
+        b.push(a.pop())
+if disks % 2 == 0:
+    while len(C) != disks:
+        move(A, B)
+        move(A, C)
+        move(B, C)
+else:
+    while len(C) != disks:
+        move(A, C)
+        move(A, B)
+        move(B, C)
+while not C.is_empty():
+    print(C.pop())
