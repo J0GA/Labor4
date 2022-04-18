4701a00463179825cdfe5c0c3ca8c9327b6c3519
Stas <SLn00200182@gmail.com>
2022-04-18 21:07:29 +0300

    Task 6

--- main.py
+++ main.py
@@ -219,3 +219,28 @@ def check_square_brackets(string):
     return bracket_stack.is_empty()
 print(check_square_brackets('[[[]]]'))
 print(check_square_brackets('[[][][]'))
+
+print()
+print("Задание №6")
+text = '12Ранд  ом-ные_симв@лы!32!54!'
+letters = Stack()
+digits = Stack()
+others = Stack()
+for c in text:
+    if c.isalpha():
+        letters.push(c)
+    elif c.isdigit():
+        digits.push(c)
+    else:
+        others.push(c)
+new_text = ''
+letters.reverse()
+digits.reverse()
+others.reverse()
+while not digits.is_empty():
+    new_text += digits.pop()
+while not letters.is_empty():
+    new_text += letters.pop()
+while not others.is_empty():
+    new_text += others.pop()
+print(new_text)
