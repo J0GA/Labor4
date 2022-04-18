c73064d8ca79dbc58e4b4303da15fff0ea9fe2a4
Stas <SLn00200182@gmail.com>
2022-04-18 17:23:07 +0300

    Task 2

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import random
+
 class LinkedNode:
     def __init__(self, value=None):
         self.value = value
         self.right = None
         self.left = None
 
-
 # Стек
 class Stack:
     def __init__(self):  # инициализация
         self.head = LinkedNode()
         self.size = 0
-
     def is_empty(self):  # is_empty - определить, пусто ли оно
         return self.size == 0
-
     def push(self, value):  # push - добавляет элемент в верхнюю часть стека
         if self.size > 0:
             node = LinkedNode(value)
@@ -22,7 +21,6 @@ class Stack:
         else:
             self.head.value = value
         self.size += 1
-
     def pop(self):  # pop - удаляет элемент в верхней части стека
         if self.is_empty():
             return print("Стек пустой!")
@@ -31,20 +29,16 @@ class Stack:
             self.head = remove.right
         self.size -= 1
         return remove.value
-
     def peek(self):  # peek - возвращается к верхнему элементу стека
         if self.is_empty():
             return print("Стек пустой!")
         return self.head.value
-
     def __len__(self):  # возвращает количество элементов в стеке
         return self.size
-
     def reverse(self):  # реверс
         current = self.head
         prev = None
         next = None
-
         while current is not None:
             next = current.right
             current.right = prev
@@ -53,17 +47,14 @@ class Stack:
 
         self.head = prev
 
-
 # Дек
 class Deque:
     def __init__(self):  # инициализация
         self.head = LinkedNode()
         self.tail = self.head
         self.size = 0
-
     def is_empty(self):  # is_empty - определить, пусто ли оно
         return self.size == 0
-
     def push_left(self, value):  # добавляет к началу двухсторонней очереди
         if self.size > 0:
             node = LinkedNode(value)
@@ -73,7 +64,6 @@ class Deque:
         else:
             self.tail.value = value
         self.size += 1
-
     def push(self, value):  # добавляет к концу двухсторонней очереди
         if self.size > 0:
             node = LinkedNode(value)
@@ -83,7 +73,6 @@ class Deque:
         else:
             self.head.value = value
         self.size += 1
-
     def pop_left(self):  # удаляет и возвращает элемент с левой стороны двусторонней очереди
         if self.is_empty():
             return print("Стек пустой!")
@@ -92,7 +81,6 @@ class Deque:
             self.tail = remove.right
         self.size -= 1
         return remove.value
-
     def pop(self):  # удаляет и возвращает элемент с правой стороны двусторонней очереди
         if self.is_empty():
             return print("Стек пустой!")
@@ -101,17 +89,14 @@ class Deque:
             self.head = remove.left
         self.size -= 1
         return remove.value
-
     def peek(self):  # возвращает элемент начала, не удаляя его
         if self.is_empty():
             return print("Стек пустой!")
         return self.head.value
-
     def peek_left(self):  # возвращает элемент начала, не удаляя его
         if self.is_empty():
             return print("Стек пустой!")
         return self.tail.value
-
     def __len__(self):  # возвращает количество элементов в двухсторонней очереди
         return self.size
 
@@ -130,3 +115,47 @@ with open('books.txt','r') as books:
         q2.push(x)
     while not q2.is_empty():
         print(q2.pop())
+
+print("Задание №2")
+alphabet = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+random.shuffle(alphabet)    #Перемешивает изменяемую последовательность случайным образом
+alphabet = ''.join(alphabet) #join для того, чтобы конвертировать список букв алфавита (без разделений) в строку для сохранения
+print(alphabet+" - специальный алфавит")
+key = Deque()
+for letter in alphabet:
+    key.push(letter)
+#кодировка
+def encode(c):
+    for i in range(len(key)):
+        x = key.pop_left()
+        if x == c:
+            key.push(x)
+            val = key.pop_left()
+            key.push(val)
+            return val
+        key.push(x)
+#декодировка
+def decode(c):
+    for i in range(len(key)):
+        x = key.pop()
+        if x == c:
+            key.push_left(x)
+            val = key.pop()
+            key.push_left(val)
+            return val
+        key.push_left(x)
+text = 'Это зашифрованное сообщение.'.lower()
+encoded = ''
+for letter in text:
+    if encoded_letter := encode(letter):
+        encoded += encoded_letter
+    else:
+        encoded += letter
+print(encoded)
+decoded = ''
+for letter in encoded:
+    if decoded_letter := decode(letter):
+        decoded += decoded_letter
+    else:
+        decoded += letter
+print(decoded)
