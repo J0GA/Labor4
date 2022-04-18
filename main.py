0fbbcaf8003c8b91d6857a62f201ffcc6515f16f
Stas <SLn00200182@gmail.com>
2022-04-18 17:35:00 +0300

    Task 5

--- main.py
+++ main.py
@@ -202,6 +202,20 @@ def check_brackets(string):
                 return False
             bracket_stack.pop()
     return bracket_stack.is_empty()
-
-print(check_brackets('(())()()'))
+print(check_brackets('(())())'))
 print(check_brackets('(((()())()()()()))'))
+
+print()
+print("Задание №5")
+def check_square_brackets(string):
+    bracket_stack = Deque()
+    for i in string:
+        if i == '[':
+            bracket_stack.push(i)
+        elif i == ']':
+            if bracket_stack.is_empty():
+                return False
+            bracket_stack.pop()
+    return bracket_stack.is_empty()
+print(check_square_brackets('[[[]]]'))
+print(check_square_brackets('[[][][]'))
